747a9d77165ccee3c0ef1c336337f54fe74db0ec
Lorelei Wright <wrighl@rpi.edu>
2018-10-13 18:31:52 -0400

    Added player movement and camera controller

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [Header("Object References")]
+    [SerializeField] Camera camera;
+    [SerializeField] Transform head;
+
+    [Header("Camera Parameters")]
+    [SerializeField] float orbit_distance;
+    [SerializeField] float sensitivity;
+    [SerializeField] bool invert_x;
+    [SerializeField] bool invert_y;
+
+    private float mx, my;
+    private RaycastHit hit;
+
+    private void Update()
+    {
+        mx += Input.GetAxis("Mouse X") * sensitivity * ((invert_x) ? -1f : 1f);
+        my += Input.GetAxis("Mouse Y") * sensitivity * ((invert_y) ? -1f : 1f);
+
+
+        head.localRotation = Quaternion.Euler(my, mx, 0f);
+
+        if (Physics.Raycast(head.position, -head.forward, out hit, orbit_distance))
+        {
+            camera.transform.position = hit.point;
+        }
+        else
+        {
+            camera.transform.position = (-head.forward * orbit_distance) + head.position;
+        }
+
+        camera.transform.LookAt(head);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Transform head;
+    [SerializeField] float speed;
+
+    private Vector3 dir;
+    private float h, v, f;
+
+    private bool flying;
+
+    private void Start()
+    {
+        flying = false;
+    }
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        f = Input.GetAxis("Fly");
+
+        if (flying)
+        {
+            dir = ((Vector3.ProjectOnPlane(head.forward, Vector3.up).normalized * v) + (head.right * h) + (Vector3.up * f)).normalized * speed;
+            if (Physics.Raycast(transform.position, Vector3.down, 0.1f))
+            {
+                flying = false;
+                rigidbody.useGravity = true;
+            }
+        }
+        else
+        {
+            dir = ((Vector3.ProjectOnPlane(head.forward, Vector3.up).normalized * v) + (head.right * h)).normalized * speed;
+            dir.Set(dir.x, rigidbody.velocity.y, dir.z);
+            if (f > 0f)
+            {
+                transform.position += Vector3.up;
+                flying = true;
+                rigidbody.useGravity = false;
+            }
+        }
+
+        rigidbody.velocity = dir;
+    }
+
+}
